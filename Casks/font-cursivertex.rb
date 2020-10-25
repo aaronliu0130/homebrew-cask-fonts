fea3c6fc60e0aabc2d5cd0be35113ab31a80153d
Baron Liu <baronliulue@outlook.com>
2020-10-25 08:26:47 +0800

    Add font-cursivertex Cask latest

---
+++ Casks/font-cursivertex.rb
@@ -0,0 +1,10 @@
+cask 'font-inconsolata' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://get.fontspace.co/download/family/wlgjp/9679bba7843e42338a98a63866fe1b64/cursivertex-font.zip'
+  name 'Cursivertex'
+  homepage 'https://www.fontspace.com/cursivertex-font-f19098'
+
+  font 'CursivertexRegular-Ox1P.ttf'
+end
